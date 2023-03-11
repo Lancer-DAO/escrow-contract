32cdb99463e6e81a100a9d061911d574cf22011a
chinepun <chokejiri@gmail.com>
2023-03-11 22:30:54 +0100

    tests for creator = submitter

--- tests/integration.ts
+++ tests/integration.ts
@@ -1223,4 +1223,260 @@ describe("integration tests", () => {
           assert.equal(acc.approvedSubmitters[2].toString(), PublicKey.default.toString());
 
   })
+
+  it ("cancel vote if creator = submitter", async () => {
+    let creator = await createKeypair(provider);
+    const submitter = creator;
+    const WSOL_AMOUNT = 1;
+    const creator_wsol_account = await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        creator,
+        WSOL_ADDRESS,
+        creator.publicKey
+    );
+    const submitter_wsol_account = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      submitter,
+      WSOL_ADDRESS,
+      submitter.publicKey
+    );
+    let convert_to_wsol_tx = new Transaction().add(
+        // trasnfer SOL
+        SystemProgram.transfer({
+          fromPubkey: provider.publicKey,
+          toPubkey: creator_wsol_account.address,
+          lamports: WSOL_AMOUNT * LAMPORTS_PER_SOL,
+        }),
+        // sync wrapped SOL balance
+        createSyncNativeInstruction(creator_wsol_account.address)
+    );
+
+    await provider.sendAndConfirm(convert_to_wsol_tx, );
+
+    const ix = await createFeatureFundingAccountInstruction(
+      WSOL_ADDRESS,
+      creator.publicKey,
+      program
+    );
+
+    const [program_authority] = await findProgramAuthority(program);
+
+    let tx = await provider.sendAndConfirm(new Transaction().add(ix), [creator]);
+    const accounts = await provider.connection.getParsedProgramAccounts(
+      program.programId, 
+      {
+        filters: [
+          {
+            dataSize: 288, // number of bytes
+          },
+          {
+            memcmp: {
+              offset: 8, // number of bytes
+              bytes: creator.publicKey.toBase58(), // base58 encoded string
+            },
+          },
+        ],      
+      }
+    );
+
+    let acc = await program.account.featureDataAccount.fetch(accounts[0].pubkey);
+    
+    const [feature_data_account] = await findFeatureAccount(
+      acc.unixTimestamp,
+      creator.publicKey,
+      program
+    );
+
+    
+    let approveSubmitterIx = await addApprovedSubmittersInstruction(
+      acc.unixTimestamp,
+      creator.publicKey,
+      submitter.publicKey,
+      program
+    )
+    
+    tx = await provider.sendAndConfirm(new Transaction().add(approveSubmitterIx), [creator]); 
+
+    // Should be false already
+    assert.equal(acc.requestSubmitted, false);
+    const submitRequestIx = await submitRequestInstruction(
+        acc.unixTimestamp, 
+        creator.publicKey, 
+        submitter.publicKey,
+        submitter_wsol_account.address,
+        program
+    )
+
+    tx = await provider.sendAndConfirm(new Transaction().add(submitRequestIx), [submitter])
+    
+    // creator cancels feature(VoteToCancel)
+    let voteToCancelIx = await voteToCancelInstruction(
+      acc.unixTimestamp,
+      creator.publicKey,
+      creator.publicKey,
+      true,
+      program
+    );
+    tx = await provider.sendAndConfirm(new Transaction().add(voteToCancelIx), [creator]);
+
+    acc = await program.account.featureDataAccount.fetch(feature_data_account);
+
+    assert.equal(acc.funderCancel, true);
+    assert.equal(acc.payoutCancel, true);
+    assert.equal(acc.requestSubmitted, false);
+
+    console.log("voteToCancel Tx(1) = ", tx);        
+
+  })
+
+  it ("test creator = submitter works perfectly when calling approveRequest", async () => {
+    let creator = await createKeypair(provider);
+
+    const WSOL_AMOUNT = 1;
+    const creator_wsol_account = await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        creator,
+        WSOL_ADDRESS,
+        creator.publicKey
+    );
+    let convert_to_wsol_tx = new Transaction().add(
+        // trasnfer SOL
+        SystemProgram.transfer({
+          fromPubkey: provider.publicKey,
+          toPubkey: creator_wsol_account.address,
+          lamports: WSOL_AMOUNT * LAMPORTS_PER_SOL,
+        }),
+        // sync wrapped SOL balance
+        createSyncNativeInstruction(creator_wsol_account.address)
+    );
+
+    await provider.sendAndConfirm(convert_to_wsol_tx, );
+
+    const create_FFA_ix = await createFeatureFundingAccountInstruction(
+      WSOL_ADDRESS,
+      creator.publicKey,
+      program
+    );
+    let tx = await provider.sendAndConfirm(new Transaction().add(create_FFA_ix), [creator]);
+    console.log("createFFA(2nd test) transaction signature", tx);
+
+    // transfer 1 WSOL
+    let amount = 1 * LAMPORTS_PER_SOL;
+    const accounts = await provider.connection.getParsedProgramAccounts(
+      program.programId, 
+      {
+        filters: [
+          {
+            dataSize: 288, // number of bytes
+          },
+          {
+            memcmp: {
+              offset: 8, // number of bytes
+              bytes: creator.publicKey.toBase58(), // base58 encoded string
+            },
+          },
+        ],      
+      }
+    );
+
+    let acc = await program.account.featureDataAccount.fetch(accounts[0].pubkey);
+
+    // Add funds to FFA token account(FundFeature)
+    let fund_feature_ix = await fundFeatureInstruction(
+      amount,
+      acc.unixTimestamp,
+      creator.publicKey,
+      WSOL_ADDRESS,
+      program
+    );
+
+      tx = await provider.sendAndConfirm(new Transaction().add(fund_feature_ix), [creator]);
+      console.log("fundFeature transaction signature", tx);
+
+      // add pubkey to list of accepted submitters(AddApprovedSubmitters)
+      const submitter1 = creator;
+      const submitter1_wsol_account = await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        submitter1,
+        WSOL_ADDRESS,
+        submitter1.publicKey
+    );
+      let addApproveSubmitterIx = await addApprovedSubmittersInstruction(
+        acc.unixTimestamp,
+        creator.publicKey,
+        submitter1.publicKey,
+        program
+      )
+      
+      tx = await provider.sendAndConfirm(new Transaction().add(addApproveSubmitterIx), [creator]); 
+
+      // test approve request fails if there is no submitted request(ApproveRequest)  
+      let [feature_data_account] = await findFeatureAccount(
+        acc.unixTimestamp,
+        creator.publicKey,
+        program
+      );    
+      let [feature_token_account] = await findFeatureTokenAccount(
+        acc.unixTimestamp,
+        creator.publicKey,
+        WSOL_ADDRESS,
+        program
+      );
+      let [program_authority] = await findProgramAuthority(program);
+  
+      try{
+        let approveRequestIx = await program.methods.approveRequest()
+        .accounts({
+          creator: creator.publicKey,
+          submitter: submitter1.publicKey,
+          payoutAccount: submitter1_wsol_account.address,
+          featureDataAccount: feature_data_account,
+          featureTokenAccount: feature_token_account,
+          programAuthority: program_authority,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        }).signers([creator]).rpc();
+      }catch(err){
+        assert.equal((err as AnchorError).error.errorMessage, "No Request Submitted yet")
+      }
+      // submit request for merging(SubmitRequest)
+      const submitRequestIx = await submitRequestInstruction(
+        acc.unixTimestamp, 
+        creator.publicKey, 
+        submitter1.publicKey,
+        submitter1_wsol_account.address,
+        program
+    )
+    acc = await program.account.featureDataAccount.fetch(accounts[0].pubkey);
+    tx = await provider.sendAndConfirm(new Transaction().add(submitRequestIx), [submitter1])
+
+      // approve request(merge and send funds)(ApproveRequest)
+        const submitter_token_account_before_balance = await provider.connection.getTokenAccountBalance(submitter1_wsol_account.address)
+        acc = await program.account.featureDataAccount.fetch(accounts[0].pubkey);
+
+        let approveRequestIx = await approveRequestInstruction(
+          acc.unixTimestamp,
+          creator.publicKey,
+          submitter1.publicKey,
+          submitter1_wsol_account.address,
+          WSOL_ADDRESS,
+          program
+        );
+        tx = await provider.sendAndConfirm(new Transaction().add(approveRequestIx), [creator])
+        console.log("approve Request tx = ", tx);
+
+        const submitter_token_account_after_balance = await provider.connection.getTokenAccountBalance(submitter1_wsol_account.address)
+        assert.equal(
+          submitter_token_account_after_balance.value.amount, 
+          (
+            amount + parseInt(submitter_token_account_before_balance.value.amount)
+          ).toString()
+        );
+
+        let closed_token_account = await provider.connection.getBalance(feature_token_account);
+        let closed_data_account = await provider.connection.getBalance(feature_data_account);
+   
+        assert.equal(0, parseInt(closed_data_account.toString()));
+        assert.equal(0, parseInt(closed_token_account.toString()));
+  })
+
 });
