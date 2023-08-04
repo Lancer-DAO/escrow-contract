0483c16ded9d50c97d71ca791a8b5f08d2d1ad54
Jo D <jo.desorm@outlook.com>
2023-08-04 09:18:11 -0400

    Changed buddylink id to mainnet

--- programs/mono-program/src/constants.rs
+++ programs/mono-program/src/constants.rs
@@ -6,7 +6,8 @@ pub const MIN_NO_OF_SUBMITTERS: usize = 1;
 pub const MONO_DATA: &str = "mono";
 pub const REFERRER: &str = "referrer";
 pub const FEE: u64 = 5;
-pub const COMPLETER_FEE: u64 = 95;//95% out of 110%
+pub const COMPLETER_FEE: u64 = 95;
+//95% out of 110%
 pub const LANCER_FEE: u64 = 10;
 // 1% out of 110% or 10% out of lancer_fee
 pub const REFERRAL_FEE: u64 = 10;
@@ -14,4 +15,5 @@ pub const REFERRAL_FEE: u64 = 10;
 pub static LANCER_ADMIN: Pubkey = pubkey!("admbKYEXAPNHZbzNADk7arzf8ZbDiZ9u4qzNS3XwRXx");
 pub const PERCENT: u64 = 100;
 pub const LANCER_DAO: &str = "LANCER_DAO";
-pub const BUDDY_LINK_PROGRAM_ID: Pubkey = pubkey!("9zE4EQ5tJbEeMYwtS2w8KrSHTtTW4UPqwfbBSEkUrNCA"); //devnet
+// pub const BUDDY_LINK_PROGRAM_ID: Pubkey = pubkey!("9zE4EQ5tJbEeMYwtS2w8KrSHTtTW4UPqwfbBSEkUrNCA"); //devnet
+pub const BUDDY_LINK_PROGRAM_ID: Pubkey = pubkey!("BUDDYtQp7Di1xfojiCSVDksiYLQx511DPdj2nbtG9Yu5"); //mainnet
